4951c320f7bd6f7dcdf5fd66644596256f1a43ac
Mathew <vinod_mathew@intuit.com>
2019-05-29 21:04:45 -0700

    a

--- geek/DP/src/main/java/App.java
+++ geek/DP/src/main/java/App.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
     public static void main(String[] args) {
@@ -13,6 +16,23 @@ public class App {
         System.out.println(fact);
         System.out.println(fact2);
         System.out.println(fact3);
+
+        Integer set[] = {3, 34, 4, 12, 5, 200,4,5,6,7,8,9,10,33,87,23,445,2,34,45,345,3432423};
+        int sum = 2000;
+        int setLen = set.length;
+
+        SumSubset ss = new SumSubset();
+
+        Map<String,Boolean> lookup = new HashMap<String, Boolean>();
+
+        long t1 = System.currentTimeMillis();
+        System.out.println(ss.subsetExistsRecursion(set,setLen,sum));
+        long t2 = System.currentTimeMillis();
+        System.out.println(ss.subSetExistsMemTopDown(set,setLen,sum,lookup));
+        long t3 = System.currentTimeMillis();
+
+        System.out.println(t2-t1);
+        System.out.println(t3-t2);
 
     }
 }
